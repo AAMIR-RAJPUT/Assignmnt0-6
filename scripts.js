4fd3a099c002e11a0163fadac4cce3545da200eb
AAMIR-RAJPUT <aamirrajput421@gmail.com>
2023-09-01 22:23:42 +0500

    work complete

---
+++ scripts.js
@@ -0,0 +1,29 @@
+
+// chapter 1
+
+//!q1
+//?alert("Hello, visitor! Welcome to our website.");
+//!q2
+//?alert("Error! Please enter a valid password");
+//!q3
+//?alert("Welcome to Js Land... \n Happy Coding!");
+//!q4
+//?alert("Welcome to Js Land.);
+//?alert("Happy Coding!");
+//!q5
+//?console.log("This is a message in the console.");
+
+
+
+// chapter 2
+//?var userName= john;
+//?alert("john")
+//?alert("15")
+//?alert("abdetg")
+
+//!q5
+//?alert("Pizza//
+//?alert("Pizz")
+//?alert("Piz")
+//?alert("Pi")
+
